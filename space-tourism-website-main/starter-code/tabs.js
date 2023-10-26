0f72d580aa3224a9e104a72c58590626d2cc29d6
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-10-26 14:15:34 -0600

    progreso en tabs y detalles

---
+++ space-tourism-website-main/starter-code/tabs.js
@@ -0,0 +1,62 @@
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
+
+tabList.addEventListener('keydown', changeTabFocus);
+
+
+let tabFocus = 0;
+
+function changeTabFocus(e) {
+    const keydownLeft = 37;
+    const keydownRight = 39;
+
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
+        tabs[tabFocus].setAttribute("tabindex", -1);
+    }
+
+    if (e.keyCode === keydownRight) {
+        tabFocus++;
+        if (tabFocus >= tabs.length) {
+            tabFocus = 0;
+        }
+    }
+
+    if (e.keyCode === keydownLeft) {
+        tabFocus = (tabFocus - 1 + tabs.length) % tabs.length;
+    }
+
+    tabs.forEach((tab, index) => {
+        const isSelected = index === tabFocus;
+        tab.setAttribute("aria-selected", isSelected);
+        tab.setAttribute("tabindex", isSelected ? 0 : -1);
+    });
+
+    tabs[tabFocus].focus();
+}
+
+
+
+
+
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', changeTabPanel);
+});
+
+
+function changeTabPanel(e) {
+    const targetTab = e.target;
+    const targetPanelId = targetTab.getAttribute("aria-controls");
+
+    const tabContainer = targetTab.parentNode;
+    const mainContainer = tabContainer.parentNode;
+
+    mainContainer
+        .querySelectorAll('[role="tabpanel"]')
+        .forEach((panel) => {
+            panel.setAttribute("hidden", true);
+        });
+
+    const targetPanel = mainContainer.querySelector(`#${targetPanelId}`);
+    targetPanel.removeAttribute('hidden');
+}
